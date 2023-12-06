28943967157f0c034c01e6b92c013d21e4da1a54
Yongjun <wangyj641@gmail.com>
2023-12-06 16:17:24 +0800

    implement pie char for personal news statist

--- src/views/sandbox/home/Home.js
+++ src/views/sandbox/home/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react'
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import axios from 'axios'
-import { Card, Col, Row, List, Avatar } from 'antd'
+import { Card, Col, Row, List, Avatar, Drawer } from 'antd'
 import * as Echarts from 'echarts'
 import _ from 'lodash'
 
@@ -9,7 +9,15 @@ const { Meta } = Card
 
 export default function Home() {
     const [viewList, setViewList] = useState([])
+    const [starList, setStarList] = useState([])
+    const [allList, setAllList] = useState([])
+    const [pieChart, setPieChart] = useState(null)
+    const [open, setOpen] = useState(false)
+
     const barRef = useRef()
+    const pieRef = useRef()
+
+    const { username, region, role: { roleName } } = JSON.parse(localStorage.getItem("token"))
 
     useEffect(() => {
         axios.get('/news?publishState=2&_expand=category&_sort=view&_order=desc&_limit=6').then(
@@ -19,8 +27,6 @@ export default function Home() {
             }
         )
     }, [])
-
-    const [starList, setStarList] = useState([])
 
     useEffect(() => {
         axios.get('/news?publishState=2&_expand=category&_sort=star&_order=desc&_limit=6').then(
@@ -35,7 +41,12 @@ export default function Home() {
         axios.get('/news?publishState=2&_expand=category').then(res => {
             //console.log(res.data)
             renderBarView(_.groupBy(res.data, item => item.category.title))
+            setAllList(res.data)
         })
+
+        return () => {
+            window.onresize = null
+        }
     }, [])
 
     const renderBarView = (obj) => {
@@ -53,9 +64,15 @@ export default function Home() {
                 data: ['Count']
             },
             xAxis: {
-                data: Object.keys(obj)
+                data: Object.keys(obj),
+                axisLabel: {
+                    rotate: "45",
+                    interval: 0
+                }
             },
-            yAxis: {},
+            yAxis: {
+                minInterval: 1
+            },
             series: [
                 {
                     name: 'Count',
@@ -66,10 +83,87 @@ export default function Home() {
         }
 
         // 使用刚指定的配置项和数据显示图表。
-        myChart.setOption(option);
+        myChart.setOption(option)
+
+        window.onresize = () => {
+            console.log("resize")
+            myChart.resize()
+        }
     }
 
-    const { username, region, role: { roleName } } = JSON.parse(localStorage.getItem("token"))
+    const renderPieView = () => {
+        var currentList = allList.filter(item => item.author === username)
+        console.log(currentList)
+        var groupObj = _.groupBy(currentList, item => item.category.title)
+        console.log(groupObj)
+
+        var list = []
+        for (var i in groupObj) {
+            list.push({
+                name: i,
+                value: groupObj[i].length
+            })
+        }
+        console.log(list)
+
+        var myChart
+
+        if (!pieChart) {
+            console.log('init echarts')
+            myChart = Echarts.init(pieRef.current)
+            setPieChart(myChart)
+        } else {
+            myChart = pieChart
+        }
+
+        var option;
+
+        option = {
+            title: {
+                text: `${username} Personal News Statist`,
+                subtext: '',
+                left: 'center'
+            },
+            tooltip: {
+                trigger: 'item'
+            },
+            legend: {
+                orient: 'vertical',
+                left: 'left'
+            },
+            series: [
+                {
+                    name: 'Access From',
+                    type: 'pie',
+                    radius: '50%',
+                    data: list,
+
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        }
+
+        option && myChart.setOption(option)
+    }
+
+    const showDrawer = () => {
+        setTimeout(() => {
+            setOpen(true);
+            renderPieView()
+        }, 0)
+    };
+
+    const onClose = () => {
+        console.log('close')
+        setOpen(false);
+    };
+
     return (
         <div className="site-card-wrapper">
             <Row gutter={16}>
@@ -110,7 +204,7 @@ export default function Home() {
                             />
                         }
                         actions={[
-                            <SettingOutlined key="setting" />,
+                            <SettingOutlined key="setting" onClick={showDrawer} />,
                             <EditOutlined key="edit" />,
                             <EllipsisOutlined key="ellipsis" />,
                         ]}
@@ -130,6 +224,18 @@ export default function Home() {
                     </Card>
                 </Col>
             </Row>
+
+            <Drawer title="Personal News Detail"
+                placement="right"
+                width="700px"
+                onClose={onClose}
+                open={open}>
+                <div ref={pieRef} style={{
+                    height: "400px",
+                    width: "100%",
+                    marginTop: "40px"
+                }}></div>
+            </Drawer>
 
             <div ref={barRef} style={{
                 height: "400px",
